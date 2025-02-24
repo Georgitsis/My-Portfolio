0a073c1c67152d926db7cd90072658ebd7f15a84
PsyChoBoyJAck83 <georgitsis.theodoros@gmail.com>
2025-02-24 15:25:20 +0100

    animations on top

--- src/Components/HeaderComponent.tsx
+++ src/Components/HeaderComponent.tsx
@@ -14,56 +14,65 @@ export default function HeaderComponent({
   const { hovered, ref } = useHover();
   // const [];
   return (
-    <Group
-      className={`${classes.NavBarElement} ${
-        startInitialTransitions ? classes.NavBarElementTransitioned : ""
-      }`}
-      justify="space-between"
-      h={"100%"}
-      maw={1280}
-      mx={"auto"}
-      px="md"
-      pb="md">
-      <Stack align="center" justify="flex-end" h={"100%"} p="sm">
-        <Group ref={ref} style={{ cursor: "pointer" }}>
-          <FaRegUser size="32" />
-          <Transition
-            mounted={hovered}
-            transition="fade-right"
-            duration={400}
-            timingFunction="ease">
-            {(styles) => (
-              <Anchor
-                underline="never"
-                style={{ ...styles, color: "inherit" }}
-                size="xl">
-                Projects
-              </Anchor>
-            )}
-          </Transition>
+    <Transition
+      mounted={startInitialTransitions}
+      transition={"slide-down"}
+      duration={800}
+      enterDelay={400}>
+      {(styles) => (
+        <Group
+          style={styles}
+          className={`${classes.NavBarElement} ${
+            startInitialTransitions ? classes.NavBarElementTransitioned : ""
+          }`}
+          justify="space-between"
+          h={"100%"}
+          maw={1280}
+          mx={"auto"}
+          px="md"
+          pb="md">
+          <Stack align="center" justify="flex-end" h={"100%"} p="sm">
+            <Group ref={ref} style={{ cursor: "pointer" }}>
+              <FaRegUser size="32" />
+              <Transition
+                mounted={hovered}
+                transition="fade-right"
+                duration={400}
+                timingFunction="ease">
+                {(styles) => (
+                  <Anchor
+                    underline="never"
+                    style={{ ...styles, color: "inherit" }}
+                    size="xl">
+                    Projects
+                  </Anchor>
+                )}
+              </Transition>
+            </Group>
+          </Stack>
+          <Stack
+            align="center"
+            justify="flex-end"
+            h={"100%"}
+            p="sm"
+            style={{
+              background: "var(--mantine-color-dark-6)",
+              borderBottomLeftRadius: "var(--mantine-radius-md)",
+              borderBottomRightRadius: "var(--mantine-radius-md)",
+            }}>
+            <Anchor
+              href="mailto:georgitsis.theodoros@gmail.com"
+              style={{
+                color: "inherit",
+                display: "inline-flex",
+                alignItems: "center",
+              }}>
+              <FaRegMessage size="32" />
+            </Anchor>
+          </Stack>
         </Group>
-      </Stack>
-      <Stack
-        align="center"
-        justify="flex-end"
-        h={"100%"}
-        p="sm"
-        style={{
-          background: "var(--mantine-color-dark-6)",
-          borderBottomLeftRadius: "var(--mantine-radius-md)",
-          borderBottomRightRadius: "var(--mantine-radius-md)",
-        }}>
-        <Anchor
-          href="mailto:georgitsis.theodoros@gmail.com"
-          style={{
-            color: "inherit",
-            display: "inline-flex",
-            alignItems: "center",
-          }}>
-          <FaRegMessage size="32" />
-        </Anchor>
-      </Stack>
-    </Group>
+      )}
+    </Transition>
   );
 }
 /* 
--- src/Components/NameBanner.tsx
+++ src/Components/NameBanner.tsx
@@ -1,6 +1,16 @@
 import { useState } from "react";
-import { Stack, Image, Text, Box, Center } from "@mantine/core";
+import {
+  Stack,
+  Image,
+  Text,
+  Box,
+  Center,
+  Group,
+  Anchor,
+  Transition,
+} from "@mantine/core";
 import classes from "./ComponentStyles.module.css";
+import { LiaHandPointRightSolid } from "react-icons/lia";
 
 interface BannerComponentProps {
   startInitialTransitions: boolean;
@@ -37,12 +47,48 @@ export default function BannerComponent({
             }`}>
             THEODOROS GEORGITSIS
           </Text>
-          <Text
-            className={`${classes.BannerSubText} ${
-              startInitialTransitions ? classes.BannerSubTextTransitioned : ""
-            }`}>
-            Full Stack Developer
-          </Text>
+          <Transition
+            mounted={startInitialTransitions}
+            transition="slide-up"
+            enterDelay={400}
+            duration={800}>
+            {(styles) => (
+              <Group
+                justify="space-between"
+                w="100%"
+                style={{
+                  ...styles,
+                  position: "relative",
+                  top: "calc(-1 * clamp(1px, 5.65vw, 70px))",
+                }}>
+                <Text
+                  className={`${classes.BannerSubText} ${
+                    startInitialTransitions
+                      ? classes.BannerSubTextTransitioned
+                      : ""
+                  }`}>
+                  Full Stack Developer
+                </Text>
+                <Group>
+                  <LiaHandPointRightSolid className={`${classes.Finger}`} />
+                  <Anchor
+                    className={`${classes.BannerSubText}`}
+                    href="mailto:georgitsis.theodoros@gmail.com"
+                    underline="never"
+                    px={"xs"}
+                    style={{
+                      color: "inherit",
+                      border: "solid 1px white",
+                      borderRadius: "var(--mantine-radius-xl)",
+                      display: "inline-flex",
+                      alignItems: "center",
+                    }}>
+                    Get in touch
+                  </Anchor>
+                </Group>
+              </Group>
+            )}
+          </Transition>
         </>
       )}
     </Stack>
