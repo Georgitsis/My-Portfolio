b9e377e65cfa78c14f0b833ebfd382010fb01d42
PsyChoBoyJAck83 <georgitsis.theodoros@gmail.com>
2025-02-24 12:44:55 +0100

    mailto Link styles

--- src/Components/HeaderComponent.tsx
+++ src/Components/HeaderComponent.tsx
@@ -1,4 +1,4 @@
-import { Group, Stack } from "@mantine/core";
+import { Group, Stack, Anchor } from "@mantine/core";
 import { FaRegMessage } from "react-icons/fa6";
 import { FaRegUser } from "react-icons/fa";
 import classes from "./ComponentStyles.module.css";
@@ -27,8 +27,25 @@ export default function HeaderComponent({
           <FaRegUser size="32" />
         </Group>
       </Stack>
-      <Stack align="center" justify="flex-end" h={"100%"} p="md">
-        <FaRegMessage size="32" />
+      <Stack
+        align="center"
+        justify="flex-end"
+        h={"100%"}
+        p="sm"
+        style={{
+          background: "var(--mantine-color-dark-6)",
+          borderBottomLeftRadius: "var(--mantine-radius-md)",
+          borderBottomRightRadius: "var(--mantine-radius-md)",
+        }}>
+        <Anchor
+          href="mailto:georgitsis.theodoros@gmail.com"
+          style={{
+            color: "inherit",
+            display: "inline-flex",
+            alignItems: "center",
+          }}>
+          <FaRegMessage size="32" />
+        </Anchor>
       </Stack>
     </Group>
   );
